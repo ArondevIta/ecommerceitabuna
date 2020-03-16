5312c6f69cb394f867d7319deb379b2b6bb28f29
Aron Madson de Tarso Sousa Amaral <ba20395388@internal.cambuci.com.br>
2020-03-16 11:26:40 -0300

    teste de arquivo

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "staticfiles")
 ]
 
 # Update database configuration with $DATABASE_URL
