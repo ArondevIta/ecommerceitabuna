6bb186a931d1dc769edb264c35dd535594fecd13
Aron Madson de Tarso Sousa Amaral <ba20395388@internal.cambuci.com.br>
2020-03-16 14:54:29 -0300

    static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 # Update database configuration with $DATABASE_URL
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
