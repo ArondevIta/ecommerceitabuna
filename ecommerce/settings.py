7907dee68d6afa2a1e3016e4ebbc062d49c5d156
Aron Madson de Tarso Sousa Amaral <ba20395388@internal.cambuci.com.br>
2020-03-16 13:17:18 -0300

    -84

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "core/static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "core/static")
+# ]
 
 # Update database configuration with $DATABASE_URL
 db_from_env = dj_database_url.config(conn_max_age=500)
